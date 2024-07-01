1b5567f0c97ba7631b4dd0cef398fa28d686cf3a
Kopano Majara <kops4jc@gmail.com>
2024-07-01 14:34:36 -0400

    updates after function to build both charts

--- static/js/app.js
+++ static/js/app.js
@@ -28,28 +28,73 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    var samples = data.samples; 
 
     // Filter the samples for the object with the desired sample number
-
+    var sampleResult = samples.find(samp => samp.id == sample);
 
     // Get the otu_ids, otu_labels, and sample_values
-
+    var otuIds = sampleResult.otu_ids; 
+    var sampleValues = sampleResult.sample_values; 
+    var otuLabels = sampleResult.otu_labels; 
+    
 
     // Build a Bubble Chart
+    //Createa layout for the BubbleChart
+    var bubbleLayout = {
+        title: "Bacteria Cultures Per Sample",
+        margin: {t:0}, 
+        showlegend: false, 
+        xaxis: {title: "OTU ID"},
+        yaxis: {title: "Number of Bacteria"},
+        hovermode: "closest",
+        //margin: {t:30}
+      };
+
+    //Create the trace for the bubble Chart
+    var bubbleMetrics = [{
+        x: otuIds,
+        y: sampleValues,
+        text: otuLabels,
+        mode: "markers",
+        marker: {
+          size: sampleValues,
+          color: otuIds,
+          colorscale: "Earth"
+        }
+    }]; 
+
+    // Render the Bubble Chart using Plotly
+    Plotly.newPlot("bubble", bubbleMetrics, bubbleLayout, {responsive: true});  
 
 
-    // Render the Bubble Chart
-
-
-    // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    // For the Bar Chart, mapping the otu_ids to a list of strings for the yticks
+    //use top-10 values for y values in descending order
+    var yticks = otuIds.slice(0,10).map(otuId => "OTU " + id).reverse(); 
 
     // Build a Bar Chart
-    // Don't forget to slice and reverse the input data appropriately
+    var barMetrics = [{
+      x: sampleValues.slice(0,10).reverse(),
+      y: yticks,
+      text: otuLabels.slice(0,10).reverse(),
+      type: "bar",
+      orientation: 'h'
+    }];
 
+    //Create the layout for the bar
+    var barLayout = [{
+      title: "Top 10 Bacteria Cultures Found",
+      margin: {t: 30, l: 150},
+      yaxis: {
+        tickmode: "array",
+        tickvals: [0,1,2,3,4,5,6,7,8,9],
+        ticktext: yticks
+      },
+
+    }];
 
     // Render the Bar Chart
+    Plotly.newPlot("bar", barMetrics, barLayout); 
 
   });
 }
